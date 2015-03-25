27ce23ff7c71f67ffbfdcca6062376b014495b91
Marc Pare <marcpare0@gmail.com>
2015-03-25 01:51:46 -0700

    Initial plotting of expenses

--- app/components/app/app-view.js
+++ app/components/app/app-view.js
@@ -1,3 +1,4 @@
+var moment = require('moment');
 var AmpersandView = require('ampersand-view');
 var AmpersandCollection = require('ampersand-collection');
 var Transactions = require('../../services/transactions');
@@ -7,6 +8,8 @@ var AppView = AmpersandView.extend({
   
   initialize: function () {
     this.transactions = [];
+    this.expenses = [];
+    this.expenseHeaders = [];
     this.headers = [];
     
     // attach to DOM right away
@@ -28,9 +31,59 @@ var AppView = AmpersandView.extend({
   },
   
   render: function () {
+    
+    // calculateExpenses
+    
+    var expenses = this.transactions.map(function (t) {
+      return [moment(t.Date), parseFloat(t.Amount), t['Transaction Type']];
+    });
+    
+    expenses = expenses.filter(function (e) {
+      return e[2] === 'debit';
+    });
+    
+    expenses = expenses.map(function (e) {
+      return [e[0], e[1]];
+    });
+    
+    expenses.sort(function (a, b) {
+      return a[0].unix()-b[0].unix();
+    });
+    
+    var cumsum = 0;
+    expenses = expenses.map(function (e) {
+      cumsum += e[1];
+      return [e[0], cumsum];
+    });
+    
+    this.expenses = expenses;
+    this.expenseHeaders = [
+      'Date',
+      'Amount'
+    ];
+    
+    
+    
     this.renderWithTemplate();    
     
-    if (this.transactions.length > 0) $('[data-hook=transactions]').DataTable();
+    if (this.transactions.length > 0) {
+      
+      $('[data-hook=expenses]').DataTable({
+        lengthMenu: [[-1], ['All']]
+      });
+      
+      $('[data-hook=transactions]').DataTable({
+        lengthMenu: [[-1], ['All']]
+      });
+      
+      var d = expenses;
+      console.log(d);
+
+      $.plot("#placeholder", [d], {
+        xaxis: { mode: "time" }
+      });
+      
+    }     
   }
 });
 module.exports = AppView;
