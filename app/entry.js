4bc860b45f2f304d7cf61f01815d28360d163bf7
Marc Pare <marcpare0@gmail.com>
2015-03-29 00:38:08 -0700

    Tweaks to build environment
    - Properly source the .env
    - Open dev tools on startup

--- app/entry.js
+++ app/entry.js
@@ -7,3 +7,4 @@ This is the entry-point for node-webkit.
 // So that we can use Node's require in Browersify'd modules:
 noderequire = require;
 nodeprocess = process;
+require('nw.gui').Window.get().showDevTools()
